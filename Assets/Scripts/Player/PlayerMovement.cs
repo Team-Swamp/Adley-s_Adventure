afbf007451cc8aab7e7a5e578ee287b5b0dffbbf
bas-boop <dereusbas461@gmail.com>
2022-07-21 15:12:00 +0200

    Added on start walking event

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -9,9 +10,24 @@ public class PlayerMovement : MonoBehaviour
     
     [Header("Value's")]
     [SerializeField] private float walkSpeed;
+    [SerializeField] private Vector3 startMoveThreshold = new Vector3(0, 0, 0);
+
+    [Header("Bool's")]
+    private bool _isStartMoving = true;
+
+    [Header("Event Variables")]
+    [SerializeField] private UnityEvent onStartMoving = new UnityEvent();
     
     public void Move(Vector2 dir)
     {
         rb.velocity = new Vector3(dir.x * walkSpeed, rb.velocity.y, dir.y * walkSpeed);
+
+        if (rb.velocity == startMoveThreshold) _isStartMoving = true;
+        
+        if (rb.velocity != startMoveThreshold && _isStartMoving)
+        {
+            onStartMoving?.Invoke();
+            _isStartMoving = false;
+        }
     }
 }
