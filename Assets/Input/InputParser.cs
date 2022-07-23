e68a59f7cfaa82d104c01830caf6dc2764e5600a
bas-boop <70578065+bas-boop@users.noreply.github.com>
2022-07-23 17:43:07 +0200

    Revert "feature/health-systeem"

--- Assets/Input/InputParser.cs
+++ Assets/Input/InputParser.cs
@@ -3,16 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.InputSystem.Processors;
 
 public class InputParser : MonoBehaviour
 {
     private PlayerInput _playerInput;
     private InputActionAsset _playerControlsActions;
-
+    
     [Header("Scripts")] 
     [SerializeField] private PlayerMovement playerMovement;
-    [SerializeField] private HealthData playerHealthData;
 
     private void Awake()
     {
@@ -24,7 +22,5 @@ public class InputParser : MonoBehaviour
     {
         var moveInput = _playerControlsActions["Move"].ReadValue<Vector2>();
         playerMovement.Move(moveInput);
-        
-        _playerControlsActions["TakeDamage"].started += (InputAction.CallbackContext context) => playerHealthData.TakeDamage(2);
     }
 }
